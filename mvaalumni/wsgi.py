2dd40c90b13ca8bf8aeb64ef16029c257e90e5e8
Michel Blancard <michel.blancard@data.gouv.fr>
2016-06-29 22:35:06 +0200

    fix bug in wsgi conf

--- mvaalumni/wsgi.py
+++ mvaalumni/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mvaalumni.settings_uwsgi")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mvaalumni.settings_wsgi")
 
 application = get_wsgi_application()
